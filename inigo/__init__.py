854b468ca51d1222913e4e5f6e1bab7706e188c2
Benjamin Bengfort <benjamin@bengfort.com>
2015-08-14 16:32:33 -0400

    version bump to 0.2.1

--- inigo/__init__.py
+++ inigo/__init__.py
@@ -20,7 +20,7 @@ Tools for dealing with images on disk for archival purposes
 __version_info__ = {
     'major': 0,
     'minor': 2,
-    'micro': 0,
+    'micro': 1,
     'releaselevel': 'final',
     'serial': 1,
 }
